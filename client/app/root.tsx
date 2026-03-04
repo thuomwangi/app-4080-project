3f1d337061ae9b5164612f78290f3ce42fca55e9
thuomwangi <thuomwangi.a@gmail.com>
2026-03-04 12:27:36 +0300

    installed bootstrap

--- client/app/root.tsx
+++ client/app/root.tsx
@@ -10,6 +10,8 @@ import {
 import type { Route } from "./+types/root";
 import "./app.css";
 
+import "bootstrap/dist/css/bootstrap.min.css";
+
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
   {
