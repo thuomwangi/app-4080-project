a80ef2172ee12869d141bf5134e8a14bec1b54f2
thuomwangi <thuomwangi.a@gmail.com>
2026-03-04 18:09:30 +0300

    added middleware + controllers + models

---
+++ server/models/product.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    description: {
+        type: String,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;
