83702af30bfa4945adb40a2dcf57fecf9dc5a7d1
thuomwangi <thuomwangi.a@gmail.com>
2026-03-04 12:51:33 +0300

    routing base

--- client/app/routes.ts
+++ client/app/routes.ts
@@ -1,3 +1,12 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+    index("routes/home.tsx"),
+    route("products", "routes/product-listing.tsx"),
+    route("products/:id", "routes/product-detail.tsx"),
+    route("seller-dashboard", "routes/seller-dashboard.tsx"),
+
+    //catches error 404 
+    route("*", "routes/not-found.tsx"),
+
+] satisfies RouteConfig;
---
+++ client/app/routes/seller-dashboard.tsx
@@ -0,0 +1,13 @@
+export default function productDetail() {
+  return (
+    <div className="container py-5">
+      <h1 className="mb-4">Seller Dashboard</h1>
+      <div className="alert alert-info">
+        This is a simple seller dashboard.
+      </div>
+      <a href="/" className="btn btn-outline-secondary">
+        ← Back to Home
+      </a>
+    </div>
+  );
+}
